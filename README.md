12c8de3823cb9fdf01339a71c5336d7f262e2da9
Perockzii <perockzii@gmail.com>
2023-11-07 22:34:16 -0800

    make betty happy

---
+++ README.md
@@ -0,0 +1,1 @@
+building my very own shell
---
+++ name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a beautiful betty compliant code that prints my name
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Hi, My name is Brice!!!\n");
+	return (0);
+}
